3a4c715ff543352fb9945031cf297fc0897d3779
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2023-12-05 12:39:42 +0100

    Solution for Day 3

--- Day3/ProgramDay3.py
+++ Day3/ProgramDay3.py
@@ -1,4 +1,9 @@
+#####################
+#     Part One      #
+#####################
+
 import re
+
 
 # function to test if a character is a special character
 def is_special_character(char):
@@ -36,3 +41,54 @@ with open('input.txt', 'r') as f:
                 number = ""
 
 print(f"Sum of every number adjacent to a symbol: {total_sum}")
+
+
+#####################
+#     Part Two      #
+#####################
+
+
+def find_gear_positions(row, col):
+    positions = [[0, -1], [-1, 0], [1, 0], [0, 1], [1, 1], [-1, -1], [1, -1], [-1, 1]]
+    adjacent_positions = []
+
+    for x, y in positions:
+        if (row + x) in range(len(lines)) and (col + y) in range(len(lines[0])):
+            if lines[row + x][col + y] == "*":
+                adjacent_positions.append([row + x, col + y])
+    return adjacent_positions
+
+
+with open('input.txt') as f:
+    lines = f.readlines()
+
+    total_gear_ratio = 0
+    gear_data = [[[1, 0]] * len(lines[0]) for _ in range(len(lines))]
+    for j in range(len(lines)):
+        line = lines[j]
+        num = ""
+        flag = []
+        for i in range(len(line)):
+            character = line[i]
+
+            if character.isdigit():
+                num += character
+                flag.extend(find_gear_positions(j, i))
+            else:
+                visit = []
+                for x in flag:
+                    if x not in visit:
+                        visit.append(x)
+                if len(num) > 0 and len(visit) > 0:
+                    for x, y in visit:
+                        gear_data[x][y] = [gear_data[x][y][0] * int(num), gear_data[x][y][1] + 1]
+                    print(i, j, gear_data[x][y])
+                flag = []
+                num = ""
+
+    for i in range(len(gear_data)):
+        for j in range(len(gear_data[0])):
+            if gear_data[i][j][1] == 2:
+                total_gear_ratio += gear_data[i][j][0]
+
+    print(total_gear_ratio)
